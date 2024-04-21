b10ad093913ed2fd15e96ea2af68d1eb8ffb09fb
victoryK <user.kemelevictory3802@gmail.com>
2024-04-21 22:30:54 +0100

    sidebar

--- port.js
+++ port.js
@@ -1,11 +1,30 @@
 const header = document.querySelector("header");
-let menu = document.querySelector("menu-icon");
+let menuIcon = document.getElementById("menu-icon");
 let navigation = document.querySelector("navigation");
-
+let navHome = document.getElementById("nav-home");
+let closetBtn = document.getElementById("bx-x");
+let bodyWhole = document.getElementById("wrapper");
 window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", window.scrollY > 120);
 });
 
-menu.addEventListener("click", () => {
-  navigation.classList.toggle("active");
+menuIcon.addEventListener("click", () => {
+  navHome.style.visibility = "visible";
+  menuIcon.style.visibility = "hidden";
+  closetBtn.style.visibility = "visible";
+  bodyWhole.style.filter = "blur(-20)";
+});
+
+// window.onscroll = () => {
+//   menu.classList.remove("bx-x");
+//   navigation.classList.remove("active");
+// };
+// menu.addEventListener("click", () => {
+//   navigation.classList.toggle("active");
+// });
+
+closetBtn.addEventListener("click", () => {
+  navHome.style.visibility = "hidden";
+  closetBtn.style.visibility = "hidden";
+  menuIcon.style.visibility = "visible";
 });
